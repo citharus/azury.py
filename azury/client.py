204ac56be7702a175c5241cea82b1482cccca1e7
citharus <citharus@protonmail.com>
2021-06-20 19:40:46 +0200

    :construction: Added Client

---
+++ azury/client.py
@@ -0,0 +1,82 @@
+#  Copyright 2021-present citharus
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any, Optional, Type
+
+import aiohttp
+from yarl import URL
+
+from azury import __version__
+
+__all__: list[str] = ["Client"]
+
+
+class Client:
+    def __init__(
+            self,
+            token: str,
+            *,
+            connector: Optional[aiohttp.BaseConnector] = None,
+            session: Optional[aiohttp.ClientSession] = None
+    ) -> None:
+        self.url: str = "https://azury.gg/api"
+        self.token: str = token
+        self.connector: Optional[aiohttp.BaseConnector] = connector
+
+        if session is None:
+            session: aiohttp.ClientSession = aiohttp.ClientSession(
+                connector=connector,
+            )
+        self.session: aiohttp.ClientSession = session
+
+    async def __aenter__(self) -> Client:
+        return self
+
+    async def __aexit__(
+            self,
+            exc_type: Optional[Type[BaseException]],
+            exc_value: Optional[BaseException],
+            exc_traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    async def close(self) -> None:
+        await self.session.close()
+
+    async def _request(
+            self,
+            method: str,
+            service: str,
+            endpoint: list[str],
+            **params: Any,
+    ) -> aiohttp.ClientResponse:
+        url: URL = URL("/".join([self.url, service, *endpoint]))
+        params: dict = dict(**params, token=self.token)
+        headers: dict = {
+            "User-Agent": f"azury.py (https://github.com/citharus/azury.py "
+                          f"{__version__}) Python{sys.version[:5]} "
+                          f"aiohttp/{aiohttp.__version__[:5]}",
+        }
+
+        response: aiohttp.ClientResponse = await self.session.request(
+            method,
+            url,
+            params=params,
+            headers=headers
+        )
+        return response
