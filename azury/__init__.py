e1e1743a312f1ffd5562d7c1bebcaed19b3dec30
citharus <citharus@protonmail.com>
2021-06-20 20:00:15 +0200

    :fire: Removed __path__

--- azury/__init__.py
+++ azury/__init__.py
@@ -18,6 +18,4 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-present citharus'
 __version__ = '0.0.1'
 
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
 from .client import *
