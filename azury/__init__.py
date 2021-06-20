e755251d01758b01e2252c1f9567658142ac509b
citharus <citharus@protonmail.com>
2021-06-20 19:44:54 +0200

    :sparkles: Added additional info

--- azury/__init__.py
+++ azury/__init__.py
@@ -12,6 +12,12 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from .client import *
+__title__ = 'azury'
+__author__ = 'citharus'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present citharus'
+__version__ = '0.0.1'
 
-__version__: str = "0.0.1"
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .client import *
