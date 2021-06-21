2e4e2655c187354e1e1ae0e72deb677a52d542b8
citharus <citharus@protonmail.com>
2021-06-21 20:54:25 +0200

    :sparkles: Added asynczury

--- azury/__init__.py
+++ azury/__init__.py
@@ -21,7 +21,8 @@ __version__ = '0.0.1a'
 import logging
 from collections import namedtuple
 
-from aiozury.client import *
+from azury import asynczury
+
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 version_info = VersionInfo(major=0, minor=0, micro=1, releaselevel='alpha', serial=0)
