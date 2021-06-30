7079f8f98c68028d17114c830e749254cd483ef2
citharus <citharus@protonmail.com>
2021-06-30 19:01:03 +0200

    :label: Changed request return type

--- azury/asynczury/client.py
+++ azury/asynczury/client.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 import asyncio
 import logging
 from types import TracebackType
-from typing import Any, Optional, Type
+from typing import Any, Optional, Type, Union
 
 import aiohttp
 import sys
@@ -119,7 +119,7 @@ class Client:
             service: str,
             endpoint: list[str],
             **params: Any,
-    ) -> Any:
+    ) -> Union[dict, list]:
         url: URL = URL('/'.join([self.base, service, *endpoint]))
         params: dict = dict(**params, token=self.token)
 
@@ -135,7 +135,7 @@ class Client:
             service: str,
             endpoint: list[str],
             **params: Any,
-    ) -> Any:
+    ) -> Union[dict, list]:
         return await self._request('GET', service, endpoint, **params)
 
     async def _post(
@@ -143,7 +143,7 @@ class Client:
             service: str,
             endpoint: list[str],
             **params: Any,
-    ) -> Any:
+    ) -> Union[dict, list]:
         return await self._request('POST', service, endpoint, **params)
 
     async def _put(
@@ -151,7 +151,7 @@ class Client:
             service: str,
             endpoint: list[str],
             **params: Any,
-    ) -> Any:
+    ) -> Union[dict, list]:
         return await self._request('PUT', service, endpoint, **params)
 
     async def _delete(
